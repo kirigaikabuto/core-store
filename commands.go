10e81ed471e0b28584714f03a2d956e3b1dbce8f
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-14 18:22:02 +0600

    sdsddd

--- commands.go
+++ commands.go
@@ -4,7 +4,7 @@ type ListMoviesCommand struct {
 	Count int64 `json:"count,omitempty"`
 }
 
-func (cmd *ListMoviesCommand) Exec(service UserService) (interface{}, error) {
+func (cmd *ListMoviesCommand) Exec(service CoreService) (interface{}, error) {
 	return service.ListMovies(cmd)
 }
 
@@ -12,7 +12,7 @@ type GetMovieByNameCommand struct {
 	Name string `json:"name"`
 }
 
-func (cmd *GetMovieByNameCommand) Exec(service coreService) (interface{}, error) {
+func (cmd *GetMovieByNameCommand) Exec(service CoreService) (interface{}, error) {
 	return service.GetMovieByName(cmd)
 }
 
@@ -20,7 +20,7 @@ type GetMovieByIdCommand struct {
 	Id int64 `json:"id"`
 }
 
-func (cmd *GetMovieByIdCommand) Exec(service coreService) (interface{}, error) {
+func (cmd *GetMovieByIdCommand) Exec(service CoreService) (interface{}, error) {
 	return service.GetMovieById(cmd)
 }
 
